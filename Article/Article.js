316da2bf2506e7fc79e4f4d79d2f1189f5f6b146
Justin Trombley <jtrombley96@gmail.com>
2019-05-15 13:31:58 -0400

    MVP

--- Article/Article.js
+++ Article/Article.js
@@ -1,22 +1,24 @@
 // Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
-
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
+
     // create a reference to the ".expandButton" class. 
-    this.expandButton.addEventListener('click', () => {
-      this.expandArticle()
-    });
+    this.expandButton = domElement.querySelector('.expandButton');
+
     // Using your expandButton reference, update the text on your expandButton to say "expand"
+    this.expandButton.textContent = "expand";
 
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-
+    this.expandButton.addEventListener("click", () =>
+      this.expandArticle());
   }
 
-  expandArticle(event) {
+  expandArticle() {
+
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    domElement.classList.toggle(Article)
+    this.domElement.classList.toggle("article-open")
   }
 }
 
@@ -31,6 +33,7 @@ in each article as a parameter to the Article class.
 */
 
 let articles = document.querySelectorAll('.article');
-articles.forEach(function (domElement) {
+articles.forEach(item => {
+  new Article(item)
 
 })
